a4f90c1b829776903e4f9494bec8d84e5c6b3ce1
Andy Fang <andy@mingtak.com.tw>
2018-12-17 09:47:38 +0000

    update 1217-1

--- src/core/2018event/browser/views.py
+++ src/core/2018event/browser/views.py
@@ -10,6 +10,8 @@ from zope.interface import alsoProvides
 import base64
 import requests
 import logging
+from selenium import webdriver
+import os
 
 logger = logging.getLogger("core.2018event")
 
@@ -24,9 +26,20 @@ class End(BrowserView):
 
         portal = api.portal.get()
         if not self.request.get('HTTP_REFERER', '').startswith(portal.absolute_url()):
-            self.request.response.redirect(portal.absolute_url())
+            self.request.response.redirect('%s?openExternalBrowser=1' % portal.absolute_url())
             return
 
+        """
+        import pdb; pdb.set_trace()
+#        browser = webdriver.PhantomJS()
+        browser = webdriver.Firefox()
+        url = request.URL
+        browser.set_window_size(400, 700)
+        browser.get(url)
+
+        browser.save_screenshot("/tmp/test.png")
+        browser.close()
+        """
         self.keys = {}
         self.keys['hat'], self.keys['scarf'], self.keys['phone'], self.keys['hand'], self.keys['clothes'], \
             self.keys['shoes'], self.keys['decoration'], self.keys['who'], self.keys['say'] = request.PATH_INFO.split('/')[-10:-1]
@@ -43,7 +56,7 @@ class Talk(BrowserView):
 
         portal = api.portal.get()
         if not self.request.get('HTTP_REFERER', '').startswith(portal.absolute_url()):
-            self.request.response.redirect(portal.absolute_url())
+            self.request.response.redirect('%s?openExternalBrowser=1' % portal.absolute_url())
             return
 
         self.keys = {}
@@ -61,7 +74,7 @@ class Image(BrowserView):
 
         portal = api.portal.get()
         if not self.request.get('HTTP_REFERER', '').startswith(portal.absolute_url()):
-            self.request.response.redirect(portal.absolute_url())
+            self.request.response.redirect('%s?openExternalBrowser=1' % portal.absolute_url())
             return
         return self.template()
 
